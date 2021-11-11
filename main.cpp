f5016848708df16c097713f5cee1923ec4d66eae
Carter Valandra-Moore <93291329+Raz2wag@users.noreply.github.com>
2021-11-11 22:57:44 +0000

    Variable example

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@ using namespace std;
 int main() 
 {
   char grade = 'A';
-
-
-
+  string phrase = "Giraffe Academy";
+  int age = 50; 
+  double gpa = -2.3;
+  bool isMale = false;
+  cout << grade << phrase << age << gpa << isMale << endl;
   return 0;
 }
 
