fdfa4f15c55acf7bd43f44f7e61c731467f7b4a5
Owen Flores <65323376+OGflo@users.noreply.github.com>
2021-11-11 23:00:02 +0000

    n

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() 
 {
-  char grade = 'A';
-  string phrase = "Giraffe Academy";
-  int age = 50; 
-  double gpa = -2.3;
-  bool isMale = false;
-  cout << grade << phrase << age << gpa << isMale << endl;
-  return 0;
+string phrase = "Giraffe Academy";
+                 0123456789
+cout << phrase[12];   
 }
-
-
+//fucking anal niggers
