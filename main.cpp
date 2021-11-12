041d04f11370bf3d551a7570092537eee68fd326
Raz2wag <93291329+Raz2wag@users.noreply.github.com>
2021-11-12 17:30:49 +0000

    Guessing Game

--- main.cpp
+++ main.cpp
@@ -3,12 +3,28 @@ using namespace std;
 
 int main()
 {
-  int index = 6;
-  do{
-    cout << index << endl;
-    index++;
-  }while(index <= 5);
+  
+  int secretNum = 7;
+  int guess;
+  int guessCount = 0;
+  int guessLimit = 3;
+  bool outOfGuesses = false;
 
+  while(secretNum != guess && !outOfGuesses){
 
+    if(guessCount < guessLimit){
+      cout << "Enter guess: ";
+      cin >> guess;
+      guessCount++;
+    } else {
+      outOfGuesses = true;
+    }
+  }
+
+  if(outOfGuesses){
+    cout << "You ran out of guesses.";
+  } else {
+  cout << "You Win!";
+  }
   return 0;
 }
