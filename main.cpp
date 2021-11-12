abb0c097f3b94fea83b9af987c97d78d683c8e6e
Raz2wag <93291329+Raz2wag@users.noreply.github.com>
2021-11-12 18:11:06 +0000

    Comments

--- main.cpp
+++ main.cpp
@@ -2,19 +2,13 @@
 using namespace std;
 
 int main(){
+  // A single line coment
+  
+  cout << "Comments are fun"; 
 
-  int numberGrid[3][2] = {
-    {1, 2},
-    {3, 4},
-    {5, 6},    
-  };
-
-  for(int i = 0; i < 3; i++){
-    for(int j = 0; j < 2; j++){
-      cout << numberGrid[i][j];
-    }
-    cout << endl;
-  }
+  /*
+  A block Comment
+  */  
 
   return 0;
 }
