378b8ed0b498786c1368abbac86150f762f18731
Raz2wag <93291329+Raz2wag@users.noreply.github.com>
2021-11-12 17:56:09 +0000

    exponential calculator

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
 
+
+int power(int baseNum, int powNum){
+  int result = 1;
+  for(int i = 0; i < powNum; i++){
+    result = result * baseNum;
+  }
+  return result;
+}
+
 int main()
 {
   
-  int nums[] = {1, 2, 5, 7, 3};
-  for(int i = 0; i < 5; i++){
-    cout << nums[i] << endl;
-  }
-
+  cout << power(4, 2);
+  
   return 0;
 }
