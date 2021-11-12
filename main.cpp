0a3116eca8294e6e813c7f3d96f391ae0b07feda
Carter Valandra-Moore <93291329+Raz2wag@users.noreply.github.com>
2021-11-12 02:18:32 +0000

    fuctions with fuction stub

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
+void sayHi (string name, int age);
+
 int main() 
 {
-
-  int luckyNums[20];
-    
-  luckyNums[0] = 100;
-
-  cout << luckyNums[0];
-
+  
+  sayHi("Mike", 60);
+  sayHi("Tom", 45);
+  sayHi("Steve", 19);
 
   return 0;
+}
+
+void sayHi(string name, int age){
+  cout << "Hello " << name << " you are " << age << endl;
 }
