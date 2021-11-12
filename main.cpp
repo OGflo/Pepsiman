b2759c61fbed048454a7f37beb8a8f4b0c75e146
Raz2wag <93291329+Raz2wag@users.noreply.github.com>
2021-11-12 15:09:22 +0000

    Advanced Calculator

--- main.cpp
+++ main.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 using namespace std;
-
-int getMax(int num1, int num2, int num3){
-  int result;
-  if(num1 >= num2 && num1 >= num3){
-    result = num1;
-  } else if (num2 >= num1 && num2 >= num3) {
-    result = num2;
-  } else {
-    result = num3;
-  }
-
-  return result;
-}
-
-int main() 
+int main()
 {
-  cout << getMax(10, 10, 5);
+
+  int num1, num2;
+  char op;
+
+  cout << "Enter first number: ";
+  cin >> num1;
+
+  cout << "Enter operator: ";
+  cin >> op;
+  
+  cout << "Enter second number: ";
+  cin >> num2;
+
+  int result;
+
+  if(op == '+'){
+    result = num1 + num2;
+  } else if(op == '-') {
+    result = num1 - num2;
+  } else if(op == '/') {
+    result = num1 / num2;
+  } else if(op == '*') {
+    result = num1 * num2;
+  } else {
+    cout << "Invalid operator";
+  }
+  
+  cout << result;
+
   return 0;
 }
