b8b44f8a079c6f229e6378a2ef945730bb9d929f
Owen Flores <65323376+OGflo@users.noreply.github.com>
2021-11-18 20:00:13 +0000

    literaly spelled std wrong dumbass

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
-using namespace std;
+#include <stdio.h> 
+#include <stdlib.h>
+#include <time.h>
 
-int main(){
-  // A single line coment
-  
-  cout << "Comments are fun"; 
+int main ()
+{
+  int iSecret, iGuess, TimesGuessed;
+  srand (time(NULL));
 
-  /*
-  A block Comment
-  */  
+  iSecret = rand() % 80 + 1;
 
+  do {
+    printf ("Guess the number (1 to 80): ");
+    scanf ("%d",&iGuess);
+    if (iSecret<iGuess) puts ("Lower");
+    else if (iSecret>iGuess) puts ("Higher");
+    TimesGuessed++;
+  } while (iSecret!=iGuess);
+
+  puts ("You Win!");
+  std::cout << "You Guessed " << TimesGuessed << "." << std::endl;
   return 0;
 }
